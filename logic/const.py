7c1fe2edc8b71b3c9e8e80732ffd88bb7a3e434c
Krzysiek <338171@uwr.edu.pl>
2023-06-12 16:52:49 +0200

    feature

--- logic/const.py
+++ logic/const.py
@@ -1,3 +1,5 @@
 from collections import namedtuple
 
 SIDES=namedtuple('SIDES', ["LEFT", "UP", "RIGHT", "DOWN"])(1, 2, 3, 4)
+
+FEATURE_TYPES=namedtuple('FEATURE_TYPES', ["CITY", "PENNANT_CITY", "ROAD", "FARM"])(1, 2, 3, 4)
--- logic/feature.py
+++ logic/feature.py
@@ -1,13 +1,22 @@
-
-
+from typing import Sequence
 
 class Connection(object):
-    def __init__(self, side, number) -> None:
+    def __init__(self, side: int, number: int) -> None:
         self.side=side
         self.number=number
         
+    def rotate_once(self) -> None:
+        self.side+=1
+        self.side%=4
 
 class Feature(object):
-    def __init__(self, _type, connections):
+    def __init__(self, _type: int, connections: Sequence[Connection]) -> None:
         self.type = _type
         self.connections = connections
+
+    def rotate(self, times: int) -> None:
+        for connection in self.connections:
+            connection.rotate_once()
+        
+        if times != 0:
+            self.rotate(times - 1)
