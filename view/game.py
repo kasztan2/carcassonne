9028a1727a864ac0f9c150c6a9f5bfa2a6b0639a
Krzysiek <338171@uwr.edu.pl>
2023-06-14 20:42:33 +0200

    .

--- view/game.py
+++ view/game.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.locals import *
+import pygame_gui as gui
 from view.scenes import WelcomeScene
 
 
@@ -17,17 +18,29 @@ class GameView(object, metaclass=Singleton):
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode((800, 800), RESIZABLE)
+        pg.display.set_caption("Carcassonne game")
         self.running = True
+
+        self.clock = pg.time.Clock()
+        self.ui_manager = gui.UIManager((800, 800))
+
         self.scenes = [WelcomeScene(self)]
         self.scene = self.scenes[0]
 
     def run(self):
+        time_delta = self.clock.tick(60) / 1000.0
         while self.running:
             for event in pg.event.get():
                 if event.type == QUIT:
                     self.running = False
+                self.ui_manager.process_events(event)
+                self.scene.process_events(event)
+
+            self.ui_manager.update(time_delta)
 
             self.scene.draw()
+
+            self.ui_manager.draw_ui(self.screen)
 
             pg.display.update()
 
--- view/scenes.py
+++ view/scenes.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from pygame.locals import *
 import pygame as pg
+import pygame_gui as gui
+from pygame_gui.elements.ui_text_entry_line import UITextEntryLine
 from view.ui_widgets import TextWidget
 
 
@@ -13,6 +15,10 @@ class Scene:
     def draw(self):
         pass
 
+    @abstractmethod
+    def process_events(self, event):
+        pass
+
 
 class WelcomeScene(Scene):
     def __init__(self, parent):
@@ -21,9 +27,40 @@ class WelcomeScene(Scene):
             parent, "Welcome to the Carcassonne Game!", 50, (0, 0), (0, 0, 0)
         )
 
-    def draw(self):
         self.parent.screen.fill(self.background_color)
+        self.numberOfPlayers = UITextEntryLine(
+            relative_rect=pg.rect.Rect(200, 200, 300, 300),
+            manager=self.parent.ui_manager,
+        )
+        self.numberOfPlayers.set_allowed_characters("numbers")
+        self.data = {"numPlayers": 0, "names": []}
+        self.phase = 0
 
+    def draw(self):
         self.welcomeText.draw()
 
         pg.display.flip()
+
+    def validateNumber(self):
+        print(self.numberOfPlayers.get_text())
+        if self.numberOfPlayers.get_text() == "":
+            return False
+
+        num = int(self.numberOfPlayers.get_text())
+        if num >= 2 and num <= 5:
+            return True
+
+        return False
+
+    def process_events(self, event):
+        if event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                if self.phase == 0:
+                    if not self.validateNumber():
+                        return
+
+                    self.data["numPlayers"] = int(self.numberOfPlayers.get_text())
+                    self.phase = 1
+                    # del self.numberOfPlayers
+                    print(self.data["numPlayers"])
+                    self.parent.ui_manager.clear_and_reset()
