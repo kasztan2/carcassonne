4daec16036f6c259651d30ac12e1e4e17817e49f
Krzysiek <338171@uwr.edu.pl>
2023-06-19 23:08:06 +0200

    docs

--- view/utils.py
+++ view/utils.py
@@ -13,7 +13,7 @@ def alignMousePosition(mousePosition, screenSize, tileSize) -> tuple[int, int]:
 
 
 def deCornify(pos: tuple[float, float], tilesize: int) -> tuple[float, float]:
-    """In fact: deEgify. Used to move a meeple away from the edge (to be visible as a whole)"""
+    """In fact: deEgify. Used to move a meeple away from the edge of a tile (to be visible as a whole)"""
     middle = (tilesize / 2, tilesize / 2)
     if pos[0] == 0 or pos[0] == tilesize or pos[1] == 0 or pos[1] == tilesize:
         return tuple(np.average([pos, middle], weights=[0.8, 0.2], axis=0))
